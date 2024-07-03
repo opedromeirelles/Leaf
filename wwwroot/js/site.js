a3f4eb9b72bfc807bffcaf9e05c9d1e3105d37a1
Pedroka <pedroa_ugusto@hotmail.com>
2024-07-03 18:03:34 -0300

    Criação do menu lateral

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//ANIMAÇÃO MENU
+const list = document.querySelectorAll('.list');
+function activeLink(){
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+    item.addEventListener('click', activeLink));
